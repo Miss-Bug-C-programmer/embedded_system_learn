6c42e93c08a9b6c3cb09a25aa7b24647dfd6ff9e
Your Name <you@example.com>
2021-04-23 15:48:27 +0800

    malloc

---
+++ c/day0b/alloctest.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+动态整型数组 
+ */
+
+int addElement(int **arr, int *n, int elm);
+void travalArray(const int *arr, int n);
+int main(void)
+{
+	int *p = NULL;
+	int n = 0;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+		addElement(&p, &n, i);
+
+	travalArray(p, n);
+
+	return 0;
+}
+
+/* 向数组中增加一个新的元素
+arr:存储数组首地址指针变量的地址
+*arr动态数组的首地址
+n:存储数组成员个数变量的地址
+elm:插入元素
+return :
+	0成功 -1失败
+*/
+int addElement(int **arr, int *n, int elm)
+{
+	*arr = realloc(*arr, (*n+1) * sizeof(int));
+	if (NULL == *arr)
+		return -1;
+	(*arr)[*n] = elm;
+	(*n)++;	
+
+	return 0;
+}
+
+// 遍历整型数组
+void travalArray(const int *arr, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
+int delArray(int **arr, int *n, int key)
+{
+	// 找
+	int i;
+
+	for (i = 0; i < *n; i++) {
+		if ((*arr)[i] == key)
+			break;
+	}
+	if (i == *n)
+		return -1;
+	// i就是待删除元素的下标
+	for (; i < *n-1; i++) {
+		(*arr)[i] = (*arr)[i+1];
+	}
+	--*n;
+	*arr = realloc(*arr, (*n) * sizeof(int));
+	// if error
+
+	return 0;
+}
+
+
+
