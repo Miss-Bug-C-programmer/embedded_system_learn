4f41087b8fedbf69137754c9c49c6fb3d24fd198
Your Name <you@example.com>
2021-06-02 14:53:12 +0800

    cleanup

--- apue/pthread/create.c
+++ apue/pthread/create.c
@@ -4,15 +4,40 @@
 #include <unistd.h>
 #include <string.h>
 
+// 线程终止处理程序
+static void exit_handler1(void *args)
+{
+	printf("%s is called\n", __FUNCTION__);
+}
+
+static void exit_handler2(void *args)
+{
+	printf("%s is called\n", __FUNCTION__);
+}
+
+static void exit_handler3(void *args)
+{
+	printf("%s is called\n", __FUNCTION__);
+}
+
 // 线程函数
 static void *thrs(void *s)
 {
-	for (int i = 0; i < 3; i++) {
+	pthread_cleanup_push(exit_handler1, NULL);
+	pthread_cleanup_push(exit_handler2, NULL);
+	pthread_cleanup_push(exit_handler3, NULL);
+
+	for (int i = 0; i < 10; i++) {
 		write(1, "!", 1);
 		sleep(1);
 	}
 
-	return NULL;
+	pthread_cleanup_pop(1);
+
+	return NULL; // 从启动例程返回
+	// pthread_exit(NULL); // (void *)0
+	pthread_cleanup_pop(0);
+	pthread_cleanup_pop(0);
 }
 
 int main(void)
@@ -30,6 +55,8 @@ int main(void)
 		sleep(1);
 	}
 
-	exit(0);
+//	exit(0);
+	pthread_exit(NULL);
 }
+
 
