1ae422b0160249198a4deb81a32fd023c0db6259
Your Name <you@example.com>
2021-06-16 14:42:20 +0800

    server

--- apue/netradio/server/main.c
+++ apue/netradio/server/main.c
@@ -1,18 +1,64 @@
 #include <stdio.h>
+#include <pthread.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+
+#include <mainproto.h>
 
 #include "medialib.h"
+#include "thrChnList.h"
+
+int serverSd;
+
+int socketInit()
+{
+	struct ip_mreqn imr;
+
+	serverSd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (-1 == serverSd) {
+		perror("socket()");
+		return -1;
+	}
+	
+	inet_aton(MULTICASTIP, &imr.imr_multiaddr);
+	inet_aton("0.0.0.0", &imr.imr_address);
+	imr.imr_ifindex = if_nametoindex("ens33");
+	if (setsockopt(serverSd, IPPROTO_IP, IP_MULTICAST_IF, &imr, sizeof(imr)) == -1) {
+		perror("setsockopt()");
+		close(serverSd);
+		return -1;
+	}
+
+	return 0;
+}
 
 int main(void)
 {
 	mlibChnList_t *mlib;
 	int chnnr;
 
+	// 初始化套接字
+	socketInit();
+
 	mlibGetChnList(&mlib, &chnnr);
 
+	// 启动发送频道列表线程
+	thrChnListStart(mlib, chnnr);
+
+#if 0
 	for (int i = 0; i < chnnr; i++) {
 		printf("%d %s\n", mlib[i].chnid, mlib[i].descr);
 	}
+#endif
 
-	exit(0);
+	while (1)
+		pause();
+
+	// exit(0);
+	pthread_exit(NULL);
 }
---
+++ apue/netradio/server/thrChnList.c
@@ -0,0 +1,67 @@
+#include <sys/socket.h>
+#include <stdio.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <pthread.h>
+#include <mainproto.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "thrChnList.h"
+
+static const mlibChnList_t *mlib;
+static int chnCnt;
+extern int serverSd;
+
+static void *thrList(void *s)
+{
+	struct list_st *list;	
+	struct listEntry_st *entry;
+	int size = 0;
+	int sum = 0;
+	struct sockaddr_in groupAddr;
+	int i;
+
+	list = malloc(MSGSIZE);
+
+	list->chnId = 0;
+	sum = sizeof(list->chnId);
+	for (i = 0; i < chnCnt; i++) {
+		size = sizeof(struct listEntry_st) + strlen(mlib[i].descr);
+		entry = malloc(size);
+		entry->chnId = mlib[i].chnid;
+		strcpy(entry->dscr, mlib[i].descr);
+		entry->len = size;
+		sum += size;
+		memcpy((char *)list+sum, entry, size);	
+		free(entry);
+	}
+	printf("sum:%d\n", sum);
+	list = realloc(list, sum);
+	
+	groupAddr.sin_family = AF_INET;
+	inet_aton(MULTICASTIP, &groupAddr.sin_addr);
+	groupAddr.sin_port = htons(RCVPORT);
+	while (1) {
+		sendto(serverSd, list, sum, 0, (void *)&groupAddr, sizeof(groupAddr));
+		sleep(1);
+	}
+}
+
+int thrChnListStart(const mlibChnList_t *ml, int cnt)
+{
+	mlib = ml;	
+	chnCnt = cnt;
+	pthread_t tid;
+	int err;
+	
+	if ((err = pthread_create(&tid, NULL, thrList, NULL)) != 0) {
+		fprintf(stderr, "pthread_create():%s\n", strerror(err));
+		return -1;
+	}
+	pthread_detach(tid);
+}
+
+
